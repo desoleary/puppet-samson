8837c70c851737acbfe8e0a7eec8ff938abceb44
Samson Nguyen <samsnguy@Samsons-MacBook-Pro.local>
2017-02-21 12:03:40 -0700

    Add chrome,and rubymine

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,15 +25,15 @@ class samson(
     ensure => installed,
   }
 
-  package { ['1password','spotify','atom','tunnelblick','spectacle','the-unarchiver']:
+  package { ['1password','spotify','atom','tunnelblick','spectacle','the-unarchiver','rubymine','google-chrome']:
     ensure   => installed,
     provider => 'brewcask'
   }
 
 ### Rubymine 7.1.4
-  package { 'https://raw.githubusercontent.com/caskroom/homebrew-cask/64f1f6b7578b3823f0a3d405206c95938adfa733/Casks/rubymine.rb':
-    provider => 'brewcask'
-  }
+#  package { 'https://raw.githubusercontent.com/caskroom/homebrew-cask/64f1f6b7578b3823f0a3d405206c95938adfa733/Casks/rubymine.rb':
+#    provider => 'brewcask'
+#  }
 
   file_line { 'ensure we load the env':
     line => 'source /opt/boxen/env.sh',
